89a6f20cc8da751ac74e225452feaf8c90d79e70
Diego Garcia <diego_9696c@hotmail.com>
2021-08-19 10:51:14 +0200

    Added Enums lesson

---
+++ types/enums.ts
@@ -0,0 +1,22 @@
+(() => {
+
+    enum ValidQualifications {
+        one = 1,
+        two,
+        three,
+        four,
+        five,
+        six,
+        seven,
+        eight,
+        nine,
+        ten
+    };
+
+    let qualification:ValidQualifications  = ValidQualifications.five;
+
+    console.log(qualification);
+    console.log(ValidQualifications);
+
+
+})()
